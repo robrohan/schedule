6ecea2a2d40112f919cf2aaac32b06964f0802b4
Rob Rohan <rob@therohans.com>
2018-02-13 17:38:31 +1100

    Updating publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-# aws s3 sync --region us-west-2 --cache-control max-age=604800 dist s3://schedule.robrohan.com
+aws s3 sync --region us-east-1 --cache-control max-age=604800 dist s3://bloodynipples.com
